2043aa6cfd4ac1827f08bc060d94e21d644b59ae
mitto nagisa <mitto@mittostar.info>
2013-01-12 16:30:03 +0900

    SkypeManagerを追加

---
+++ SkypeCallManager/SkypeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Growl_for_Skype_Notification;
+
+namespace SkypeCallManager
+{
+    public class SkypeManager : SkypeManagerBase
+    {
+        public SkypeManager()
+        {
+        }
+    }
+}
