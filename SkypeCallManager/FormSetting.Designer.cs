593208e9fa204771a3608af06cb29cff17e934be
mitto nagisa <mitto@mittostar.info>
2013-01-12 21:38:17 +0900

    設定フォームの外観を作成と設定ファイルの追加

--- SkypeCallManager/FormSetting.Designer.cs
+++ SkypeCallManager/FormSetting.Designer.cs
@@ -28,11 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.toolStripContainerMain = new System.Windows.Forms.ToolStripContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageGeneralSetting = new System.Windows.Forms.TabPage();
+            this.dateTimePickerStopTime = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.toolStripContainerMain.ContentPanel.SuspendLayout();
+            this.toolStripContainerMain.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageGeneralSetting.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainerMain
+            // 
+            // 
+            // toolStripContainerMain.ContentPanel
+            // 
+            this.toolStripContainerMain.ContentPanel.Controls.Add(this.panel1);
+            this.toolStripContainerMain.ContentPanel.Controls.Add(this.panel2);
+            this.toolStripContainerMain.ContentPanel.Size = new System.Drawing.Size(284, 262);
+            this.toolStripContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainerMain.Name = "toolStripContainerMain";
+            this.toolStripContainerMain.Size = new System.Drawing.Size(284, 262);
+            this.toolStripContainerMain.TabIndex = 0;
+            this.toolStripContainerMain.Text = "toolStripContainer1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControlMain);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 223);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageGeneralSetting);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(284, 223);
+            this.tabControlMain.TabIndex = 0;
+            // 
+            // tabPageGeneralSetting
+            // 
+            this.tabPageGeneralSetting.Controls.Add(this.dateTimePickerStopTime);
+            this.tabPageGeneralSetting.Controls.Add(this.label1);
+            this.tabPageGeneralSetting.Location = new System.Drawing.Point(4, 22);
+            this.tabPageGeneralSetting.Name = "tabPageGeneralSetting";
+            this.tabPageGeneralSetting.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageGeneralSetting.Size = new System.Drawing.Size(276, 197);
+            this.tabPageGeneralSetting.TabIndex = 0;
+            this.tabPageGeneralSetting.Text = "全般";
+            this.tabPageGeneralSetting.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePickerStopTime
+            // 
+            this.dateTimePickerStopTime.CustomFormat = "HH:mm";
+            this.dateTimePickerStopTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStopTime.Location = new System.Drawing.Point(209, 6);
+            this.dateTimePickerStopTime.Name = "dateTimePickerStopTime";
+            this.dateTimePickerStopTime.ShowUpDown = true;
+            this.dateTimePickerStopTime.Size = new System.Drawing.Size(64, 19);
+            this.dateTimePickerStopTime.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "この時間になったら通話を止める";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonOK);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 223);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(284, 39);
+            this.panel2.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonOK.Location = new System.Drawing.Point(0, 0);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(284, 39);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "閉じる";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // FormSetting
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.toolStripContainerMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSetting";
             this.Text = "FormSetting";
+            this.toolStripContainerMain.ContentPanel.ResumeLayout(false);
+            this.toolStripContainerMain.ResumeLayout(false);
+            this.toolStripContainerMain.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageGeneralSetting.ResumeLayout(false);
+            this.tabPageGeneralSetting.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainerMain;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageGeneralSetting;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStopTime;
+        private System.Windows.Forms.Label label1;
+
     }
 }
