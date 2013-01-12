287189b46f0d3daafb709a5075f0e7707fc37b64
mitto nagisa <mitto@mittostar.info>
2013-01-12 16:40:06 +0900

    メインメニューを追加

--- SkypeCallManager/FormMain.Designer.cs
+++ SkypeCallManager/FormMain.Designer.cs
@@ -28,20 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStripContainerMain = new System.Windows.Forms.ToolStripContainer();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelpVersion = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFileSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripContainerMain.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainerMain.SuspendLayout();
+            this.menuStripMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStripContainerMain
+            // 
+            // 
+            // toolStripContainerMain.ContentPanel
+            // 
+            this.toolStripContainerMain.ContentPanel.Size = new System.Drawing.Size(284, 236);
+            this.toolStripContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainerMain.Name = "toolStripContainerMain";
+            this.toolStripContainerMain.Size = new System.Drawing.Size(284, 262);
+            this.toolStripContainerMain.TabIndex = 0;
+            this.toolStripContainerMain.Text = "toolStripContainer1";
+            // 
+            // toolStripContainerMain.TopToolStripPanel
+            // 
+            this.toolStripContainerMain.TopToolStripPanel.Controls.Add(this.menuStripMain);
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemHelp});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(284, 26);
+            this.menuStripMain.TabIndex = 0;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFileSetting,
+            this.toolStripSeparator1,
+            this.toolStripMenuItemFileExit});
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            this.toolStripMenuItemFile.Size = new System.Drawing.Size(85, 22);
+            this.toolStripMenuItemFile.Text = "ファイル(&F)";
+            // 
+            // toolStripMenuItemFileExit
+            // 
+            this.toolStripMenuItemFileExit.Name = "toolStripMenuItemFileExit";
+            this.toolStripMenuItemFileExit.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItemFileExit.Text = "終了(&X)";
+            // 
+            // toolStripMenuItemHelp
+            // 
+            this.toolStripMenuItemHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemHelpVersion});
+            this.toolStripMenuItemHelp.Name = "toolStripMenuItemHelp";
+            this.toolStripMenuItemHelp.Size = new System.Drawing.Size(75, 22);
+            this.toolStripMenuItemHelp.Text = "ヘルプ(&H)";
+            // 
+            // toolStripMenuItemHelpVersion
+            // 
+            this.toolStripMenuItemHelpVersion.Name = "toolStripMenuItemHelpVersion";
+            this.toolStripMenuItemHelpVersion.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItemHelpVersion.Text = "バージョン情報(&A)...";
+            // 
+            // toolStripMenuItemFileSetting
+            // 
+            this.toolStripMenuItemFileSetting.Name = "toolStripMenuItemFileSetting";
+            this.toolStripMenuItemFileSetting.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItemFileSetting.Text = "設定(&S)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.toolStripContainerMain);
+            this.MainMenuStrip = this.menuStripMain;
             this.Name = "FormMain";
             this.Text = "FormMain";
+            this.toolStripContainerMain.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainerMain.TopToolStripPanel.PerformLayout();
+            this.toolStripContainerMain.ResumeLayout(false);
+            this.toolStripContainerMain.PerformLayout();
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainerMain;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFileExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelp;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelpVersion;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFileSetting;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
 
