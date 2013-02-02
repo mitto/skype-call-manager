f772829341003dfd3c733a118abff66e19908326
mitto nagisa <mitto@mittostar.info>
2013-02-02 15:47:34 +0900

    issue #12 対応 多重起動の防止

--- SkypeCallManager/Program.cs
+++ SkypeCallManager/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace SkypeCallManager
 {
@@ -13,6 +14,11 @@ namespace SkypeCallManager
         [STAThread]
         static void Main()
         {
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("既に起動しています", Properties.Resources.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
